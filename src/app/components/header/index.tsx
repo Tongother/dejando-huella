079cc52c0b068b3f4aaeaabe3e96504ca16ed7a7
Julian Castro <141683762+Juliox14@users.noreply.github.com>
2024-10-11 13:13:11 -0600

    11/10/2024 FIX: Corregí algunos errores de estilado en header e info, además ahora se renderiza el header o la navbar dependiendo del dispositivo

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -1,3 +1,32 @@
-import { Header } from "./header";
+'use client'
 
-export default Header;
+import { useEffect, useState } from "react";
+import { Header } from "./header";
+import { Navbar } from "./navbar";
+
+const ResponsiveHeader = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  const handleResize = () => {
+    const width = window.innerWidth;
+    if (width <= 768) {
+      setIsMobile(true); 
+    } else {
+      setIsMobile(false);
+    }
+  };
+
+  useEffect(() => {
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <>
+      {isMobile ? <Navbar /> : <Header />}
+    </>
+  );
+};
+
+export default ResponsiveHeader;
