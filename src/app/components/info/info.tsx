12ff34cef951120f2601158b0e357ff29f97fea9
Cristian Alfonsin <cristiancfack133@gmail.com>
2024-10-11 01:59:55 -0600

    11/10/2024 UPDATE: Estilé el componente info, dejando pendiente las fuentes de letra

--- src/app/components/info/info.tsx
+++ src/app/components/info/info.tsx
@@ -1,15 +1,30 @@
 import Image from "next/image";
+import "./info.css"
+import chonapng from "../../../../public/info/La chona.png";
+import gerry from "../../../../public/info/gerry.png";
 const Info = ()=>{
-	return <section className="bg-slate-400 border-solid border-2 border-black">
-		<article className="flex flex-col border-solid border-2 border-black">
-			<div id="imagen" className="border-solid border-2 border-black">
-				<Image src={} alt="perrito" width={50} height={50}>
-
-				</Image>
+	return <section className="lg:py-[100px]">
+		<article className="flex flex-col lg:flex-row-reverse items-center justify-center lg:bg-[#6f7acf] lg:mb-[150px] lg:py-10">
+			<div id="imagen" className="flex justify-center py-5">
+				<div className="forma relative lg:ml-[80px]">
+					<Image src={chonapng.src} alt="perrito" fill/>
+				</div>
 			</div>
-			<div className="border-solid border-2 border-black">
-				<h1>¿Quienes pueden ser voluntarios?</h1>
-				<h2>Todas las personas mayores de edad pueden asistir a las instalaciones para conocernos y ayudarnos a limpiar nuestras patitas.</h2>
+			<div id="descripcion" className="h-[300px] w-full bg-[#6f7acf] text-[#ffff] lg:text-3xl lg:w-[520px] lg:p-0 flex flex-col justify-center px-8">
+				<h1 className="text-center text-xl mb-4"><strong>¿Quienes pueden ser voluntarios?</strong></h1>
+				<h2 className="text-center text-l">Todas las personas mayores de edad pueden asistir a las instalaciones para conocernos y ayudarnos a limpiar nuestras patitas.</h2>
+			</div>
+		</article>
+
+		<article className="flex flex-col lg:flex-row items-center justify-center lg:bg-[#E88EEF] lg:py-10">
+			<div id="imagen" className="flex justify-center py-5">
+				<div className="forma2 relative lg:mr-[80px]">
+					<Image src={gerry.src} alt="perrito" fill/>
+				</div>
+			</div>
+			<div id="descripcion" className="h-[300px] w-full bg-[#E88EEF] text-[#ffff] lg:text-3xl lg:w-[520px] lg:p-0 flex flex-col justify-center px-8">
+				<h1 className="text-center text-xl mb-4"><strong>¿Quienes pueden ser voluntarios?</strong></h1>
+				<h2 className="text-center text-l">Todas las personas mayores de edad pueden asistir a las instalaciones para conocernos y ayudarnos a limpiar nuestras patitas.</h2>
 			</div>
 		</article>
 	</section>
