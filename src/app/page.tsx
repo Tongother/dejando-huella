ce5aabed3969cbb2d870e548e16842f485f84294
Ricardo <ricardo.cruze@outlook.com>
2024-10-11 10:29:14 -0600

    11/10/2024 create: Hice el footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,16 @@ import Info from "./components/info/info";
 import Banner from "./components/banner/Banner";
 import Stats from "./components/stats";
 import Footer from "./components/footer/Footer";
+import Banner2 from "./components/banner2/Banner2";
 
 export default function Home() {
   return (
     <>
       <Info/>
-      <Banner>
-      </Banner>
+      <Banner/>
       <Stats/>
-      <Footer>
-      </Footer>
+      <Banner2/>
+      <Footer/>
     </>
   );
 }
