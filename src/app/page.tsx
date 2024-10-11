4383709e38915e1be64b8749812b0a6780cdc94e
Ricardo <ricardo.cruze@outlook.com>
2024-10-11 02:40:53 -0600

    11/10/2024 create: hice el 2do banner responsivo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
 
 import Info from "./components/info/info";
-import Banner from "./components/banner/Banner";
+import Banner2 from "./components/banner2/Banner2";
 export default function Home() {
   return (
     <>
       <Info>
         
       </Info>
-      <Banner>
-
-      </Banner>
+      <Banner2>
+      </Banner2>
     </>
   );
 }
