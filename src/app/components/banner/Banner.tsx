e92fc8ba3448588bf512b7d6c621b084eb404dae
Ricardo <ricardo.cruze@outlook.com>
2024-10-11 01:10:46 -0600

    11/10/24 add: Ahora si termine, responsivo

--- src/app/components/banner/Banner.tsx
+++ src/app/components/banner/Banner.tsx
@@ -3,12 +3,12 @@ import Link from 'next/link';
 
 const Banner = () => {
   return (
-    <section className="flex justify-around items-center bg-[#4C62D9]  rounded-none text-white w-full h-[150px] box-border">
-      <div className="flex flex-col gap-2 text-3xl font-bold">
-        <h1>¡Ayúdanos desde cerquita!</h1>
+    <section className="flex flex-col lg:flex-row justify-evenly lg:justify-around  items-center bg-[#4C62D9]  rounded-none text-white w-full h-[300px] lg:h-[150px] box-border">
+      <div className="flex flex-col gap-2 text-3xl font-bold items-center">
+        <h1 className='text-center'>¡Ayúdanos desde cerquita!</h1>
         <p className='text-lg text-center'><a className='text-white font-bold underline' href="#">Conoce el proceso de voluntariado</a></p>
       </div>
-      <Link href="#" className="bg-[#FFB602] text-white text-center no-underline font-bold text-xl ml-8 rounded-[40px] p-[25px]">Conocer más</Link>
+      <Link href="#" className="bg-[#FFB602] text-white text-center no-underline font-bold text-xl lg:ml-8 rounded-[40px] p-[25px]">Conocer más</Link>
     </section>
   );
 };
