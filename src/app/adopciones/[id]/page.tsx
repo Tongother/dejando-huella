39f5f78de0df10d976b2a283104749a1e17e7698
Gunther <nettelgunther@outlook.es>
2024-10-23 16:13:51 -0600

    23/10/24 UPDATE: Se termino la página dinamica de adopciones, se creo un nuevo slider

--- src/app/adopciones/[id]/page.tsx
+++ src/app/adopciones/[id]/page.tsx
@@ -1,10 +1,14 @@
+import Historia from "@/app/components/adopciones/Historia";
 import Perfil from "@/app/components/adopciones/Perfil";
+import Slider from "@/app/components/adopciones/slider";
 
 const PerfilAdopcion = ({ params }: { params: { id: string } }) => {
     console.log(params.id);
     return(
-        <div>
+        <div className="font-ini">
             <Perfil />
+            <Slider />
+            <Historia />
         </div>
     )
 }
--- src/app/components/adopciones/Perfil.tsx
+++ src/app/components/adopciones/Perfil.tsx
@@ -2,24 +2,23 @@ import Image from "next/image";
 
 const Perfil = () => {
     return(
-        <section className="flex flex-col justify-center items-center font">
-            <h1 className="text-xl">Mi nombre es <span className="text-[#374193]">{}</span></h1>
+        <section className="flex flex-col justify-center items-center mt-6">
+            <h1 className="text-2xl md:text-3xl font-bold">Mi nombre es <span className="text-[#374193] font-bold">{"Churrito"}</span></h1>
             <article className="relative flex flex-col items-center">
                 <Image src="/adopcion/gatito_b.png" alt="Imagen del adoptado" width={300} height={300} className="absolute -z-10" />
-                <div className="w-[300px] p-6 border bg-white mt-56 z-10">
-                    <div className="-mt-6 p-4 border-x border-b">
+                <div className="w-[300px] md:w-[500px] p-6 border border-black bg-white mt-56 rounded-3xl shadow-xl font-bold z-10">
+                    <div className="-mt-6 p-4 border-x border-b border-black">
                         <h3 className="text-center">Información</h3>
                     </div>
                     <div className="mt-4">
-                        <p>ID: {}</p>
-                        <p>Edad: {}</p>
-                        <p>Sexo: {}</p>
-                        <p>Tamaño: {}</p>
-                        <p>Personalidad: {}</p>
-
+                        <p>ID: <span className="text-gray-500 font-bold">{"Churrito"}</span></p>
+                        <p>Edad: <span className="text-gray-500 font-bold">{"1 año"}</span></p>
+                        <p>Sexo: <span className="text-gray-500 font-bold">{"Macho"}</span></p>
+                        <p>Tamaño: <span className="text-gray-500 font-bold">{"3cm"}</span></p>
+                        <p>Personalidad: <span className="text-gray-500 font-bold">{"Fiestero"}</span></p>
                         <div className="flex flex-col items-center justify-center gap-4 mt-4">
-                            <h3>¿Quieres adoptar a {}?</h3>
-                            <button className="md:w-32 xl:w-44 xl:h-12 bg-[#024BFF] hover:bg-[#0642cd] hover:scale-105 ease-out duration-300 text-white text-xl py-2 px-4 rounded-full">Adoptar</button>
+                            <h3>¿Quieres adoptar a <span className="text-[#374193] font-bold">{"Churrito"}</span>?</h3>
+                            <button className="md:w-32 xl:w-44 xl:h-12 bg-[#374193] hover:bg-[#3e4fb7] hover:scale-105 ease-out duration-300 text-white text-lg py-2 px-4 rounded-full">Adoptar</button>
                         </div>
                     </div>
                 </div>
