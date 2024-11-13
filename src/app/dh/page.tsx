e3ed8157abb9619986b7a00cc1b58f21f4590235
Gunther <nettelgunther@outlook.es>
2024-11-13 15:20:29 -0600

    13/11/24 UPDATE: Actualice las pantallas para que aparezca un gif de carga

--- src/app/dh/page.tsx
+++ src/app/dh/page.tsx
@@ -4,18 +4,29 @@ import Banner2 from "../../components/banner2/Banner2";
 import Carrusel from "../../components/carrusel/Carrusel";
 import Info from "../../components/info";
 import Image from  "next/image";
+import { Suspense } from "react";
+
+const SkeletonLoader = () => {
+  return (
+      <div className="w-full h-screen md:h-[90vh] flex justify-center items-center">
+          <Image src="/gifts/gatito.gif" width={30} height={30} alt="Cargando..." quality={100} />
+      </div>
+  );
+}
 
 export default function Home() {
   return (
     <>
-      <div className="fixed w-full h-full -z-10">
-        <Image src={"/fondo_fijo.jpg"} fill alt="Un gatito bien bonito" className="object-cover object-[70%] lg:object-[50%]"></Image>
-      </div>
-      <Carrusel activeIndex={0} />
-      <Banner/>
-      <Info/>
-      <Stats/>
-      <Banner2/>
+      <Suspense fallback={<SkeletonLoader/>}>
+        <div className="fixed w-full h-full -z-10">
+          <Image src={"/fondo_fijo.jpg"} fill alt="Un gatito bien bonito" className="object-cover object-[70%] lg:object-[50%]"></Image>
+        </div>
+        <Carrusel activeIndex={0} />
+        <Banner/>
+        <Info/>
+        <Stats/>
+        <Banner2/>
+      </Suspense>
     </>
   );
 }
