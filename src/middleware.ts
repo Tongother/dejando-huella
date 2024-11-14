ac3a1dc7fed5a3c3d8c7253e0a5cad9e77a3892f
Gunther <nettelgunther@outlook.es>
2024-11-13 18:22:37 -0600

    13/11/24 FIX: Arregle pequeños errores

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,17 @@
 import { NextResponse } from 'next/server'
-// import type { NextRequest } from 'next/server'
+import type { NextRequest } from 'next/server'
 
 
-export async function middleware(){
-    // const auth = request.cookies.has('userToken');
-    // if (request.nextUrl.pathname.startsWith('/login') && auth){
-    //     return NextResponse.redirect(new URL('/admin', request.url))
-    // }
-    // if (request.nextUrl.pathname.startsWith('/admin') && !auth){
-    //     return NextResponse.redirect(new URL('/login', request.url))
-    // }
-    // if(request.nextUrl.pathname === '/admin/formPets'){
-    //     return NextResponse.redirect(new URL('/admin/formPets/0/0', request.url))
-    // }
+export async function middleware(request: NextRequest){
+    const auth = request.cookies.has('userToken');
+    if (request.nextUrl.pathname.startsWith('/login') && auth){
+        return NextResponse.redirect(new URL('/admin', request.url))
+    }
+    if (request.nextUrl.pathname.startsWith('/admin') && !auth){
+        return NextResponse.redirect(new URL('/login', request.url))
+    }
+    if(request.nextUrl.pathname === '/admin/formPets'){
+        return NextResponse.redirect(new URL('/admin/formPets/0/0', request.url))
+    }
     return NextResponse.next()
 }
